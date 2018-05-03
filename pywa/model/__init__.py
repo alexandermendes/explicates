8ceeb27fc97cb0694141b3230ab7c9b665e68998
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-03 22:43:28 +0100

    Remove model imports in __init__.py

--- pywa/model/__init__.py
+++ pywa/model/__init__.py
@@ -6,10 +6,3 @@ import datetime
 def make_timestamp(self):
     """Return timestamp expressed in the UTC xsd:datetime format."""
     return datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-
-
-from annotation import Annotation
-from collection import Collection
-
-assert Annotation
-assert Collection
