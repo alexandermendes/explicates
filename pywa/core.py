e703a21a421f2b587d76a2edd0269e1d891a208a
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-04 00:30:34 +0100

    Setup db when app is created

--- pywa/core.py
+++ pywa/core.py
@@ -13,6 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.process_response = process_response
     configure_app(app)
+    setup_db(app)
     setup_blueprints(app)
     setup_repositories(app)
     return app
@@ -22,6 +23,7 @@ def configure_app(app):
     """Configure app."""
     app.config.from_object(default_settings)
     app.config.from_envvar('PYWA_SETTINGS', silent=True)
+    print os.environ.get('PYWA_SETTINGS')
     if not os.environ.get('PYWA_SETTINGS'):
         here = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(os.path.dirname(here), 'settings_local.py')
@@ -34,6 +36,12 @@ def configure_app(app):
     if app.config.get('SQLALCHEMY_DATABASE_TEST_URI'):
         app.config['SQLALCHEMY_DATABASE_URI'] = \
             app.config['SQLALCHEMY_DATABASE_TEST_URI']
+
+    # Enable missing Slave bind using Master node
+    if app.config.get('SQLALCHEMY_BINDS') is None:
+        print "Slave binds are misssing, adding Master as slave too."
+        app.config['SQLALCHEMY_BINDS'] = \
+            dict(slave=app.config.get('SQLALCHEMY_DATABASE_URI'))
 
 
 def setup_blueprints(app):
@@ -49,3 +57,27 @@ def setup_repositories(app):
     from pywa.repositories import CollectionRepository
     annotation_repo = AnnotationRepository(db)
     collection_repo = CollectionRepository(db)
+
+
+def setup_db(app):
+    """Setup database."""
+    def create_slave_session(db, bind):
+        if (app.config.get('SQLALCHEMY_BINDS')['slave'] ==
+                app.config.get('SQLALCHEMY_DATABASE_URI')):
+            return db.session
+        engine = db.get_engine(db.app, bind=bind)
+        options = dict(bind=engine, scopefunc=_app_ctx_stack.__ident_func__)
+        slave_session = db.create_scoped_session(options=options)
+        return slave_session
+    db.app = app
+    db.init_app(app)
+    db.slave_session = create_slave_session(db, bind='slave')
+    if db.slave_session is not db.session:
+        # flask-sqlalchemy does it already for default session db.session
+        @app.teardown_appcontext
+        def _shutdown_session(response_or_exc):  # pragma: no cover
+            if app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']:
+                if response_or_exc is None:
+                    db.slave_session.commit()
+            db.slave_session.remove()
+            return response_or_exc
