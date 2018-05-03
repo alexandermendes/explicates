d173a025b9a86e46ed6cd5e32beff29b469d1dbb
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-04 00:45:40 +0100

    Fix local settings path

--- pywa/core.py
+++ pywa/core.py
@@ -25,7 +25,7 @@ def configure_app(app):
     app.config.from_envvar('PYWA_SETTINGS', silent=True)
     if not os.environ.get('PYWA_SETTINGS'):
         here = os.path.dirname(os.path.abspath(__file__))
-        config_path = os.path.join(os.path.dirname(here), 'settings_local.py')
+        config_path = os.path.join(os.path.dirname(here), 'settings.py')
         if os.path.exists(config_path):
             app.config.from_pyfile(config_path)
     else:
