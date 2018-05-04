96b5eac737577b7fcdb3b45cd3a8cb3d41dcaf55
Alexander Mendes <alexanderhmendes@gmail.com>
2018-05-04 15:12:05 +0100

    Test get annotation endpoint

--- pywa/api.py
+++ pywa/api.py
@@ -3,7 +3,7 @@
 import json
 from flask import Blueprint, abort
 
-from pywa.core import collection_repo
+from pywa.core import collection_repo, annotation_repo
 
 
 blueprint = Blueprint('api', __name__)
@@ -15,11 +15,25 @@ def index():
     return 'The PYWA server'
 
 
-@blueprint.route('/<slug>')
-def collection(slug):
-    """Return a collection."""
-    coll = collection_repo.get_by(slug=slug)
+@blueprint.route('/<collection_slug>')
+def collection(collection_slug):
+    """Return a Collection."""
+    coll = collection_repo.get_by(slug=collection_slug)
     if not coll:
         abort(404)
 
     return coll
+
+
+@blueprint.route('/<collection_slug>/<annotation_slug>')
+def annotation(collection_slug, annotation_slug):
+    """Return an Annotation."""
+    coll = collection_repo.get_by(slug=collection_slug)
+    if not coll:
+        abort(404)
+
+    anno = annotation_repo.get_by(slug=annotation_slug, collection=coll)
+    if not anno:
+        abort(404)
+
+    return anno
