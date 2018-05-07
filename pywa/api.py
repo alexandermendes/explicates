4a4790dc79ae1d6ea3a9e27f16e3e0d57eb31c2b
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-07 23:10:38 +0100

    Begin adding page response

--- pywa/api.py
+++ pywa/api.py
@@ -40,10 +40,19 @@ def collection(collection_slug):
     if not coll:
         abort(404)
 
+    page = request.args.pop('page', None)
+    if page:
+        return respond_page(collection, int(page))
+
+    collection_dict = coll.dictize()
+    annotations = annotation_repo.filter_by(collection_key=coll.key)
+    if annotations:
+        collection_dict['first'] = url_for('')
+
     if request.method == 'POST':
         return handle_post(Annotation, annotation_repo, collection=coll)
 
-    return coll
+    return collection_dict
 
 
 @blueprint.route('/<collection_slug>/<annotation_slug>',
@@ -59,3 +68,21 @@ def annotation(collection_slug, annotation_slug):
         abort(404)
 
     return anno
+
+
+def page_response(collection, page):
+    """Respond with a Page of a Collection."""
+    collection_dict = collection.dictize()
+    page_iri = "{0}/{1}".format(collection_dict['id'], page)
+    next_uri = "{0}/{1}".format(collection_dict['id'], page + 1)
+
+    if query_str:
+        collection_dict['id'] += "?{}".format(query_str)
+        page_iri += "?{}".format(query_str)
+        next_iri += "?{}".format(query_str)
+
+    data = {
+        'id': page_iri,
+        'type': 'AnnotationPage',
+        'partOf': collection_dict
+    }
