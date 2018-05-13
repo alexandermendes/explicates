bc2a98ee3948f41f91c3bd72a51424b8846921ed
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-13 19:48:19 +0100

    Move container retrival functions to API base

--- explicates/api/base.py
+++ explicates/api/base.py
@@ -7,6 +7,7 @@ Design notes:
   Web Annotation profile, more formats may be added in future.
 """
 
+from flask import current_app
 from flask import abort, request, jsonify, make_response, url_for
 from jsonschema.exceptions import ValidationError
 from sqlalchemy.exc import IntegrityError
@@ -134,3 +135,130 @@ class APIBase(object):
         for link in links:
             link_str = '<{0}>; rel="{1}"'.format(link['url'], link['rel'])
             response.headers.add('Link', link_str)
+
+    def _get_container_preferences(self):
+        """Get container preferences.
+
+        Defaults to PreferContainedDescriptions.
+        """
+        minimal = False
+        iris = False
+        prefer = request.headers.get('Prefer')
+        if not prefer:
+            return minimal, iris
+        try:
+            _return, include = prefer.split(';')
+        except ValueError:
+            return minimal, iris
+        if _return.strip() != 'return=representation':
+            return minimal, iris
+        if not include.strip().startswith('include='):
+            return minimal, iris
+        parts = include.split('=')[1].split()
+        prefs = [part.strip('"') for part in parts]
+
+        if 'http://www.w3.org/ns/ldp#PreferMinimalContainer' in prefs:
+            minimal = True
+        if 'http://www.w3.org/ns/oa#PreferContainedIRIs' in prefs:
+            iris = True
+
+        return minimal, iris
+
+    def _get_query_params(self, iris=None):
+        """Return a copy of the request arguments.
+
+        Remove page as this will be dealt with seperately for each IRI.
+        """
+        params = request.args.copy()
+        params.pop('page', None)
+        return params.to_dict(flat=True)
+
+    def _get_container(self, collection):
+        """Return the Collection with the requested representation.
+
+        https://www.w3.org/TR/annotation-protocol/#container-representations
+        """
+        out = collection.dictize()
+        minimal, iris = self._get_container_preferences()
+        params = self._get_query_params()
+        params['iris'] = 1 if iris or params.get('iris') == '1' else None
+
+        out['id'] = self._get_iri('api.collections',
+                                  collection_id=collection.id, **params)
+
+        page = request.args.get('page')
+        if page:
+            return self._get_page(int(page), collection, partof=out, **params)
+
+        if collection.annotations:
+            if minimal:
+                out['first'] = self._get_first_page_iri(collection, **params)
+            else:
+                out['first'] = self._get_page(0, collection, **params)
+
+            last = self._get_last_page_iri(collection, **params)
+            if last:
+                out['last'] = last
+
+        return out
+
+    def _get_last_page(self, collection):
+        """Get the last page number for a Collection.
+
+        Pagination is zero-based.
+        """
+        count = len(collection.annotations)
+        per_page = current_app.config.get('ANNOTATIONS_PER_PAGE')
+        last_page = 0 if count <= 0 else (count - 1) // per_page
+        return last_page
+
+    def _get_first_page_iri(self, collection, **params):
+        """Return the IRI for the first AnnotationPage in a Collection."""
+        if collection.annotations:
+            return self._get_iri('api.collections', page=0,
+                                 collection_id=collection.id, **params)
+
+    def _get_last_page_iri(self, collection, **params):
+        """Return the IRI for the last AnnotationPage in a Collection."""
+        last_page = self._get_last_page(collection)
+        if last_page > 0:
+            return self._get_iri('api.collections', page=last_page,
+                                 collection_id=collection.id, **params)
+
+    def _get_page(self, page, collection, partof=None, **params):
+        """Return an AnnotationPage."""
+        page_iri = self._get_iri('api.collections', page=page,
+                                 collection_id=collection.id, **params)
+        data = {
+            'type': 'AnnotationPage',
+            'id': page_iri,
+            'startIndex': 0
+        }
+
+        last_page = self._get_last_page(collection)
+        if last_page > page:
+            data['next'] = self._get_iri('api.collections', page=page + 1,
+                                         collection_id=collection.id,
+                                         **params)
+
+        if partof:
+            data['partOf'] = partof
+
+        per_page = current_app.config.get('ANNOTATIONS_PER_PAGE')
+        annotations = collection.annotations[page:page + per_page]
+        items = []
+        for annotation in annotations:
+            anno_params = params.copy()
+            anno_params.pop('iris', None)
+            anno_dict = annotation.dictize()
+            anno_dict['id'] = self._get_iri('api.annotations',
+                                            collection_id=collection.id,
+                                            annotation_id=annotation.id,
+                                            **anno_params)
+            if params.get('iris'):
+                items.append(anno_dict['id'])
+            else:
+                items.append(anno_dict)
+
+        data['items'] = items
+        return data
