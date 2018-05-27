6213060290028bf199196ddd0b35e7964874945b
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-27 20:55:43 +0100

    Index body and target

--- explicates/core.py
+++ explicates/core.py
@@ -72,6 +72,7 @@ def setup_search(app):
 
 def setup_db(app):
     """Setup database."""
+    from explicates.model.indexes import indexes
     def create_slave_session(db, bind):
         slave = app.config.get('SQLALCHEMY_BINDS')['slave']
         if slave == app.config.get('SQLALCHEMY_DATABASE_URI'):
@@ -79,7 +80,12 @@ def setup_db(app):
         engine = db.get_engine(db.app, bind=bind)
         options = dict(bind=engine, scopefunc=_app_ctx_stack.__ident_func__)
         slave_session = db.create_scoped_session(options=options)
+
+        for idx in indexes:
+            idx.create(bind=engine)
+
         return slave_session
+
     db.app = app
     db.init_app(app)
     db.slave_session = create_slave_session(db, bind='slave')
