9aba6603f27d68c34bfb694895a9fa3412b69b16
Alex Mendes <alexanderhmendes@gmail.com>
2018-05-27 23:08:12 +0100

    Add test

--- test/test_model/test_collection.py
+++ test/test_model/test_collection.py
@@ -5,6 +5,7 @@ from nose.tools import *
 from base import Test, db, with_context
 
 from explicates.model.collection import Collection
+from explicates.model.annotation import Annotation
 
 
 class TestModelCollection(Test):
@@ -27,3 +28,23 @@ class TestModelCollection(Test):
         expected = url_for('api.collections', collection_id=collection.id,
                            _external=True)
         assert_equal(collection.iri, expected)
+
+    @with_context
+    def test_total_returns_n_annotations(self):
+        """Test Collection total contains number of Annotations."""
+        collection_data = {
+            'type': [
+                'AnnotationCollection',
+                'BasicContainer'
+            ]
+        }
+        anno_data = {
+            'body': 'foo',
+            'target': 'bar'
+        }
+        collection = Collection(data=collection_data)
+        annotation = Annotation(collection=collection, data=anno_data)
+        db.session.add(collection)
+        db.session.add(annotation)
+        db.session.commit()
+        assert_equal(collection.total, 1)
